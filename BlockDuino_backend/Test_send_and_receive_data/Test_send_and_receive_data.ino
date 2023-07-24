14b5c9d4e742b01d5695d934237f88c9229081b4
mounilsankar <98753425+mounilsankar@users.noreply.github.com>
2023-07-24 12:30:34 +0800

    Add files via upload

---
+++ BlockDuino_backend/Test_send_and_receive_data/Test_send_and_receive_data.ino
@@ -0,0 +1,39 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+const char* ssid = "newveena";
+const char* password = "98246192";
+
+ESP8266WebServer server(80);
+
+void handleJSONData() {
+  if (server.method() == HTTP_POST) {
+    String json = server.arg("plain"); // Get the JSON data from the request body
+    // Parse and process the received JSON data here
+    Serial.println(json);
+    server.send(200, "application/json", "{\"status\":\"ok\"}");
+  } else {
+    server.send(400, "text/plain", "Invalid request");
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("WiFi connected");
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  server.on("/jsondata", handleJSONData);
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop() {
+  server.handleClient();
+}

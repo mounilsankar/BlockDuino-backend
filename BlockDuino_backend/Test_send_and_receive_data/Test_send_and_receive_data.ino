d1ea05cc6897e708898dba0190431ab2333767da
mounilsankar <98753425+mounilsankar@users.noreply.github.com>
2023-07-24 14:57:24 +0800

    Update Test_send_and_receive_data.ino

--- BlockDuino_backend/Test_send_and_receive_data/Test_send_and_receive_data.ino
+++ BlockDuino_backend/Test_send_and_receive_data/Test_send_and_receive_data.ino
@@ -1,8 +1,8 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
-const char* ssid = "newveena";
-const char* password = "98246192";
+const char* ssid = "NETWORD_ID";
+const char* password = "NETWORK_Password";
 
 ESP8266WebServer server(80);
 
